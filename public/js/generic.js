f1024f62d6f372b03dff0f25cfb5a75d19eea1bf
Vikram Somu <vikster93@gmail.com>
2014-05-26 23:04:47 -0400

    bla

--- public/js/generic.js
+++ public/js/generic.js
@@ -93,7 +93,7 @@ $(document).ready(function(){
 		  hwaccel: false, // Whether to use hardware acceleration
 		  className: 'spinner', // The CSS class to assign to the spinner
 		  zIndex: 2e9, // The z-index (defaults to 2000000000)
-		  top: '61%', // Top position relative to parent
+		  top: '57%', // Top position relative to parent
 		  left: '20%' // Left position relative to parent
 		};
 
