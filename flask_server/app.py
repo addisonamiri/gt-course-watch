f5ada21a3fd3ddf7e73ece39dc7e39893f601428
Vikram Somu <vikster93@gmail.com>
2015-03-31 04:59:13 -0400

    added df -h support to flask

--- flask_server/app.py
+++ flask_server/app.py
@@ -2,12 +2,45 @@ from flask import Flask
 from flask import jsonify
 import shelve
 import os
+import subprocess
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+# Perform the Unix df -h command and send output as response
+@app.route("/dfh")
+def dfh():
+    proc = subprocess.Popen(["df", "-h"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    outlines = out.split('\n')
+    ret_html = ''
+
+    header_contents = ''
+    for itm in outlines[0].split(' '):
+        if itm:
+            header_contents += "<th>{0}</th>".format(itm)
+
+    header_html = "<tr>{0}</tr>".format(header_contents)
+    ret_html += header_html
+
+    for row in outlines[1:]:
+        rowitms = row.split(' ')
+        rowcontents = ''
+        for itm in rowitms:
+            if itm:
+                rowcontents += "<td>{0}</td>".format(itm)
+        rowhtml = '<tr>{0}</tr>'.format(rowcontents)
+        ret_html += rowhtml 
+
+    ret_html = "<html><body><table>{0}</table></body></html>".format(ret_html)
+
+    # Simple method just inserting BRs
+    # out = out.replace('\n', '<br>')
+    return ret_html
 
 @app.route("/peekdl")
 def peekdl():
